0a4e5dc7c9cc9c13c8a54c29a703ba34149c5fe1
aam <muhammadaam0@gmail.com>
2025-07-03 18:34:29 +0700

    first commit

--- konek.php
+++ konek.php
@@ -1,14 +1,14 @@
 
 <?php
-// date_default_timezone_set("Asia/Jakarta");
-// $servername = "localhost";   
-// $username = "aam";         
-// $password = "aam123";            
-// $dbname = "aam_shirmp";    
+date_default_timezone_set("Asia/Jakarta");
 $servername = "localhost";   
-$username = "root";         
-$password = "";            
-$dbname = "perawat_db";    
+$username = "aam";         
+$password = "aam123";            
+$dbname = "aam_monitor";    
+// $servername = "localhost";   
+// $username = "root";         
+// $password = "";            
+// $dbname = "perawat_db";    
 
 $conn = new mysqli($servername, $username, $password, $dbname);
  
